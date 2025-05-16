66f28ed2e521945b084cb3e053fc31de6a70b1b4
zhuaman0 <aitbaevaman95@gmail.com>
2025-05-16 16:19:42 +0500

    Loader: finished loader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,7 @@
 <template>
+	<div v-if="serviceStore.pageLoading" class="tw-flex tw-items-center tw-justify-center tw-min-h-screen tw-fixed tw-w-full tw-h-full tw-top-0 tw-left-0 tw-z-[1001] tw-bg-[#00000067]">
+		<v-progress-circular color="#228B6B" indeterminate></v-progress-circular>
+	</div>
   <div class="tw-flex tw-flex-col tw-min-h-screen tw-relative">
     <header>
       <BaseHeader/>
@@ -13,7 +16,9 @@
 </template>
 
 <script lang="ts" setup>
+import { useServiceStore } from '~/stores/services';
 
+const serviceStore = useServiceStore();
 </script>
 
 <style>
