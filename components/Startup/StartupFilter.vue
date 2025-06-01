5b7f69c26aadce008bf82f426cb3783f823be9e7
Satybaldy Muhammedzhan <satybaldymuhammedzhan@MacBook-Pro-Satybaldy.local>
2025-06-01 16:21:32 +0500

    main page

---
+++ components/Startup/StartupFilter.vue
@@ -0,0 +1,26 @@
+<template>
+  <aside class="tw-w-full tw-max-w-[250px] tw-space-y-4 tw-p-4 tw-bg-white tw-rounded-xl tw-shadow">
+    <div>
+      <h3 class="tw-font-semibold tw-mb-2">Индустрии</h3>
+      <div
+        v-for="industry in industries"
+        :key="industry"
+        class="tw-flex tw-items-center tw-gap-2"
+      >
+        <input type="checkbox" :id="industry" />
+        <label :for="industry" class="tw-text-sm">{{ industry }}</label>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script setup>
+const industries = [
+  'AdTech & MarTech',
+  'AgroTech',
+  'Alternative energy',
+  'AudioTech',
+  'BioTech',
+  'Autonomous driving'
+]
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,14 +10,21 @@ const { locales, setLocale } = useI18n();
 </script>
 
 <template>
-  <div>
-	<h1 class="tw-text-center">qewr</h1>
-	<h1 class="muha">adf</h1>
-	<button v-for="locale in locales" @click="setLocale(locale.code)">
-		{{ locale.name }}
-	</button>
-	<h1>{{ $t('welcome') }}</h1>
-  </div>
+	<div class="tw-my-10">
+		<MainSectionHeader/>
+		<MainCategoriesSection/>	 
+		<MainStartupOpportunities/>
+		<MainStartupCriteria/>
+		<MainFaqSection/>	
+	</div>
+	<!-- <div>
+		<h1 class="tw-text-center">qewr</h1>
+		<h1 class="muha">adf</h1>
+		<button v-for="locale in locales" @click="setLocale(locale.code)">
+			{{ locale.name }}
+		</button>
+		<h1>{{ $t('welcome') }}</h1>
+	</div> -->
 </template>
 
 <style lang="scss">
